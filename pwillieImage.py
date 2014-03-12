5b0fdc26eb7b64313bcb632a9e8a2de395b90418
Omar Nashef <omar13@meet.mit.edu>
2014-03-12 19:43:06 +0200

    WE ARE AWESOME

--- pwillieImage.py
+++ pwillieImage.py
@@ -1,16 +1,22 @@
 import pygame
-from view.py import view
-class Image (view):
+from view import View
+class Image (View):
 	def __init__(self,_rec, _main_screen,image_r):
 		super(Image,self).__init__(_rec, _main_screen)
 		self.image_r= image_r
 	def draw(self):
-		img = pygame.iomage.load(image_r)
-		main_screen.blit(image_r,_rec)
-		
+		img = pygame.image.load(self.image_r)
+		self.main_screen.blit(img,self.rec)
+	def clear(self):
+		img=pygame.Surface([int(self.rec.width),int(self.rec.height)])
+		img.fill((255,255,255))
+		main_screen.blit(img,self.rec)
 
-if __name__ == "__main__":
+'''if __name__ == "__main__":
 	main = pygame.display.set_mode((400,400))
-	main_screen.fill((255,255,255))
-	
-	
+	main.fill((255,255,255))
+	c = Image([20,20],main,'download.jpg')
+	c.draw()
+	while (True):
+		ev = pygame.event.poll()
+		pygame.display.flip()	'''
